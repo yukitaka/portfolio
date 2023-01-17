e44c0524bc4f90142a2a08c965415fe43b1faef9
Takayuki Sato <s.yukkiy@gmail.com>
2023-01-18 00:04:11 +0900

    feat: adjust title

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,16 +3,20 @@ import { styled, setup } from "goober";
 
 setup(h);
 
-const Container = styled("div")`
-  width: 200px;
+const Title = styled("h1")`
+  width: 100%;
   height: 50px;
-  color: blue;
+  color: white;
+  font-family: impact;
+  font-size: 5em;
+  text-align: center;
+  letter-spacing: -0.02em;
 `;
 
 export default function Header() {
   return (
     <header>
-      <Container>This is header</Container>
+      <Title>DOG DRIVEN ENGINEER</Title>
     </header>
   );
 }
