f6e1d706b7472e32e1f830b3ec3c8fc2bee964ac
Takayuki Sato <s.yukkiy@gmail.com>
2023-01-19 23:03:14 +0900

    feat: remove default example

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,3 @@
-import { useState } from "preact/hooks";
-import preactLogo from "./assets/preact.svg";
 import Header from "@/components/header/index";
 import "./app.css";
 
@@ -12,33 +10,12 @@ async function getPost(id: number) {
 }
 
 export function App() {
-  const [count, setCount] = useState(0);
   const post = getPost(1);
   console.log(post);
 
   return (
     <>
       <Header />
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank" rel="noreferrer">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
     </>
   );
 }
