7cf2467a162482ea37c5ccbf064424ea4f40e135
Takayuki Sato <s.yukkiy@gmail.com>
2023-01-20 23:56:07 +0900

    rafactor: indent to 4 from 2

--- src/app.tsx
+++ src/app.tsx
@@ -2,20 +2,20 @@ import Header from "@/components/header/index";
 import "./app.css";
 
 async function getPost(id: number) {
-  const data = await fetch(`http://localhost:3000/posts/${id}`)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+    const data = await fetch(`http://localhost:3000/posts/${id}`)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
-  return data;
+    return data;
 }
 
 export function App() {
-  const post = getPost(1);
-  console.log(post);
+    const post = getPost(1);
+    console.log(post);
 
-  return (
-    <>
-      <Header />
-    </>
-  );
+    return (
+        <>
+            <Header />
+        </>
+    );
 }
