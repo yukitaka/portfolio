76cb62ce7333eec5c007fbcdfa8f54631364cd8d
Takayuki Sato <s.yukkiy@gmail.com>
2023-01-21 13:24:47 +0900

    feat: display a hamburger

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,37 +5,52 @@ import { styled, setup } from "goober";
 setup(h);
 
 const Hamburger = styled("div")`
-    display: inline-block;
+  display: inline-block;
+  position: absolute;
+  box-sizing: initial;
+  right: 0;
+  top: 0;
+  margin: 3px;
+  width: 20px;
+  height: 20px;
+  padding: 18px 23px 12px;
+  line-height: @header-height;
+  text-align: center;
+  z-index: 100;
+  opacity: 0.7;
+  cursor: pointer;
+  transition: opacity 250ms ease;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  &:before {
+    content: "";
     position: absolute;
-    box-sizing: initial;
-    right: 0;
+    left: 0;
     top: 0;
-    margin: 3px;
-    width: 20px;
-    height: 20px;
-    padding: 18px 23px 12px;
-    line-height: @header-height;
-    text-align: center;
-    z-index: 100;
-    opacity: 0.7;
-    cursor: pointer;
-    transition: opacity 250ms ease;
-  
-    &:hover {
-        opacity: 1;
-    }
-  
-    &:before {
-        content: '';
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        border-radius: 40px;
-        transform-origin: center;
-        bacground: rgba(255, 255, 255, 0.2);
-        transform: scale(0);
+    width: 100%;
+    height: 100%;
+    border-radius: 40px;
+    transform-origin: center;
+    background: rgba(255, 255, 255, 0.2);
+    transform: scale(0);
+  }
+`;
+
+const HamburgerBread = styled("div")`
+  position: relative;
+  width: 20px;
+  height: 3px;
+  border-width: 1px 0;
+  margin: 0 auto 3px;
+  padding: 0;
+  background: #fff;
+  border-radius: 1px;
+  font-size: 1px;
+  transition: all 200ms ease;
+  transform-origin: 0 0;
 `;
 
 const Title = styled("h1")`
@@ -63,7 +78,12 @@ export default function Header() {
             <SubTitle>
                 <p>Enjoy engineering of the web with dogs!</p>
             </SubTitle>
-            <Hamburger open={open} onClick={toggle} />
+
+            <Hamburger open={open} onClick={toggle}>
+                <HamburgerBread />
+                <HamburgerBread />
+                <HamburgerBread />
+            </Hamburger>
         </header>
     );
 }
