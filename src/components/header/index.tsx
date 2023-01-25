33aa55b92801b5fd986025d2d5b5e174ec323cff
Takayuki Sato <s.yukkiy@gmail.com>
2023-01-26 00:48:22 +0900

    feat: move from header to home

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,34 +1,12 @@
 import { h } from "preact"
-import { styled, setup } from "goober"
 import Menu from "./menu"
 import Hamburger from "./hamburger"
-
-setup(h)
-
-const Title = styled("h1")`
-  width: 100%;
-  height: 50px;
-  margin-bottom: 0.3em;
-  color: white;
-  font-family: impact;
-  font-size: 5em;
-  text-align: center;
-  letter-spacing: -0.02em;
-`
-
-const SubTitle = styled("div")`
-  text-align: right;
-`
 
 export default function Header() {
     return (
         <header>
             <Menu />
             <Hamburger />
-            <Title>DOG DRIVEN ENGINEER</Title>
-            <SubTitle>
-                <p>Enjoy engineering of the web with dogs!</p>
-            </SubTitle>
         </header>
     )
 }
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,4 +1,30 @@
 import { h } from "preact"
+import { styled, setup } from "goober"
+
+setup(h)
+
+const Title = styled("h1")`
+  width: 100%;
+  height: 50px;
+  margin-bottom: 0.3em;
+  color: white;
+  font-family: impact;
+  font-size: 5em;
+  text-align: center;
+  letter-spacing: -0.02em;
+`
+
+const SubTitle = styled("div")`
+  text-align: right;
+`
+
 export const Home = () => {
-    return <div>Home</div>
+    return (
+        <div>
+            <Title>DOG DRIVEN ENGINEER</Title>
+            <SubTitle>
+                <p>Enjoy engineering of the web with dogs!</p>
+            </SubTitle>
+        </div>
+    )
 }
