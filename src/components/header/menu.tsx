f705aac1ab02f331cebe6c611ffe5eade6b8a40f
Takayuki Sato <s.yukkiy@gmail.com>
2023-01-26 01:15:03 +0900

    feat: add home to menu

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -1,5 +1,6 @@
 import { h } from "preact"
 import { styled, setup } from "goober"
+import { RiHome6Fill } from "react-icons/ri"
 
 setup(h)
 
@@ -14,11 +15,19 @@ const MenuItem = styled("li")`
   display: flex;
   box-sizing: border-box;
   padding: 10px;
+  a {
+    color: white;
+  }
 `
 
 export default function Menu() {
     return (
         <MenuContainer>
+            <MenuItem>
+                <a href="/">
+                    <RiHome6Fill size={22} />
+                </a>
+            </MenuItem>
             <MenuItem>ABOUT</MenuItem>
             <MenuItem>SKILLS</MenuItem>
             <MenuItem>CONTACT</MenuItem>
