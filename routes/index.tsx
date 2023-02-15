29dcbcf57c9af1c99b608f0b5fcd47df887b4edd
Takayuki Sato <s.yukkiy@gmail.com>
2023-02-16 00:16:12 +0900

    feat: add main image

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,5 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
+import { asset } from "$fresh/runtime.ts";
 import { getContent, Content } from "@/utils/contents.ts";
 import MainTitle from "@/components/title.tsx";
 import Contacts from "@/components/contacts.tsx";
@@ -11,12 +12,16 @@ export const handler: Handlers<Content[]> = {
 };
 
 export default function ContentIndexPage(props: PageProps<Content>) {
+  const imgUrl = new URL(asset("/main.png"), props.url).href;
   const content = props.data;
   return (
     <div class="max-w-screen-md px-4 pt-16 mx-auto">
       <MainTitle />
       <Contacts />
-      <div class="max-w-screen-md px-4 pt-16 mx-auto">
+      <div class="max-w-screen-md pt-8">
+        <img src={imgUrl} class="animate-fadein" />
+      </div>
+      <div class="max-w-screen-md px-4 pt-8 mx-auto">
         <p dangerouslySetInnerHTML={{ __html: content.content }} class="animate-slide" />
       </div>
     </div>
--- twind.config.ts
+++ twind.config.ts
@@ -12,10 +12,15 @@ export default {
         "slide": {
           "0%": { transform: "translateX(300px)", opacity: 0 },
           "100%": { transform: "translateX(0px)", opacity: 100 }
+        },
+        "fadein": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 100 }
         }
       },
       animation: {
-        "slide": "slide 0.5s ease-out"
+        "slide": "slide 0.5s ease-out",
+        "fadein": "fadein 1.5s ease-out"
       }
     }
   }
