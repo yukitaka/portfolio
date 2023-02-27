75b0cdad5f534e5a164b2b4596590987cbbf5700
Takayuki Sato <s.yukkiy@gmail.com>
2023-02-27 23:27:31 +0900

    feat: implement ref

--- islands/Search.tsx
+++ islands/Search.tsx
@@ -2,19 +2,26 @@ import { useState } from "preact/hooks";
 import SearchButton from "@/components/header/SearchButton.tsx";
 import SearchModal from "@/components/SearchModal.tsx";
 
-interface Data {
-  query: string;
+interface Props {
+  query: string
 }
 
-export default function Search<Data>({ query }) {
+export default function Search(props: Props) {
     const [isOpen, setOpen] = useState<boolean>(false);
 
     return (
         <>
+            <form>
+                <input type="text" name="q" value={props.query} class="text-black rounded-full" />
+            </form>
             <button type="image" class="pl-2" onClick={() => setOpen(true)}><SearchButton /></button>
 
-            <SearchModal isOpen={isOpen} setOpen={setOpen}>
-                <div>Contents</div>
+            <SearchModal
+                isOpen={isOpen}
+                setOpen={setOpen}
+                class="p-2 border-2 border-gray-100 rounded"
+            >
+                <div class="frex flex-col"><p>Contents</p></div>
             </SearchModal>
         </>
     );
