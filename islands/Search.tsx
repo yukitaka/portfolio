a7bff9e58ec07f995ae73cd6138638dff78c4d79
Takayuki Sato <s.yukkiy@gmail.com>
2023-02-28 23:13:56 +0900

    feat: add padding

--- islands/Search.tsx
+++ islands/Search.tsx
@@ -12,7 +12,7 @@ export default function Search(props: Props) {
     return (
         <>
             <form>
-                <input type="text" name="q" value={props.query} class="text-black rounded-full" />
+                <input type="text" name="q" value={props.query} class="px-3 text-black rounded-full" />
             </form>
             <button type="image" class="pl-2" onClick={() => setOpen(true)}><SearchButton /></button>
 
