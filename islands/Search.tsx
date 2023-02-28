8f3a104bf9bea1293294efe4c542d3a4365e1802
Takayuki Sato <s.yukkiy@gmail.com>
2023-02-28 23:16:44 +0900

    feat: remove unnecessary padding

--- islands/Search.tsx
+++ islands/Search.tsx
@@ -14,7 +14,7 @@ export default function Search(props: Props) {
             <form>
                 <input type="text" name="q" value={props.query} class="px-3 text-black rounded-full" />
             </form>
-            <button type="image" class="pl-2" onClick={() => setOpen(true)}><SearchButton /></button>
+            <button type="image" onClick={() => setOpen(true)}><SearchButton /></button>
 
             <SearchModal
                 isOpen={isOpen}
