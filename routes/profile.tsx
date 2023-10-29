7b0cce4960c08e26bebbea59190b5d317488a32f
Takayuki Sato <s.yukkiy@gmail.com>
2023-10-30 01:37:01 +0900

    feat: adjust screen width

--- routes/profile.tsx
+++ routes/profile.tsx
@@ -6,7 +6,7 @@ export default function ContentIndexPage(props: PageProps<Content>) {
   const imgUrl = new URL(asset("/main.png"), props.url).href;
   const content = props.data;
   return (
-    <div className="max-w-screen-md px-4 pt-16 mx-auto flex flex-wrap flex-row w-screen">
+    <div className="max-w-screen-md w-screen px-4 pt-16 mx-auto flex flex-wrap flex-row">
       <div className="block max-w-sm p-6 border-y-2 border-r-2 border-gray-200 rounded-r-lg shadow w-1/2">
         <h5 className="text-2xl font-bold tracking-tight text-white">Sato Takayuki</h5>
         <p className="mb-2 text-sm text-gray-400">佐藤 貴之</p>
