8dd8492c5d794f9eed5e2fc9dd4ace46eea079f3
Takayuki Sato <s.yukkiy@gmail.com>
2023-10-29 13:37:36 +0900

    feat: reduce some warnings

--- utils/contents.ts
+++ utils/contents.ts
@@ -1,5 +1,5 @@
 import { extract } from "$std/front_matter/yaml.ts";
-import { join } from "$std/path/posix.ts";
+import { join } from "$std/path/mod.ts";
 import { Marked } from "$gfm";
 
 const DIRECTORY = "./contents";
@@ -29,16 +29,18 @@ export async function getContent(slug: string): Promise<Content | null> {
     const md = await Deno.readTextFile(join(DIRECTORY, `${slug}.md`));
     const extracted = extract(md);
     const meta = extracted.attrs;
-    const markup = Marked.parse(extracted.body);
+    const markup = Marked.marked.parse(extracted.body);
     return {
       slug,
-      title: meta.title,
-      publishedAt: new Date(meta.published_at),
+      title: meta.title as string,
+      publishedAt: new Date(meta.published_at as string),
       content: markup
     };
   } catch(e) {
     if (e instanceof Deno.errors.NotFound) {
-      return false;
+      return null;
     }
   }
+
+  return null;
 }
